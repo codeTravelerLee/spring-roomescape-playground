8cf71ba03e27c471447b6fea6d6ebd744229dfeb
chanh <leechan7120@skku.edu>
2026-04-27 22:16:37 +0900

    feat : 2단계 구현
    Co-authored-by: 이진우 <12jack030220@gmail.com>

--- src/main/java/roomescape/RoomscapeController.java
+++ src/main/java/roomescape/RoomscapeController.java
@@ -1,12 +1,31 @@
 package roomescape;
 
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class RoomscapeController {
+
+    private final List<Reservation> reservations = new ArrayList<>();
+
     @GetMapping("/")
     public String showPage(){
         return "home";
+    }
+
+    @GetMapping("/reservation")
+    public String showReservationPage() {
+        return "reservation";
+    }
+
+    @GetMapping("/reservations")
+    @ResponseBody
+    public List<Reservation> showAllReservations() {
+        return reservations;
     }
 }
