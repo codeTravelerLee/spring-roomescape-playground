fd2ce0302817dcdfe71aa1ccb0f56b4c48fd7695
jxinwoo <12jack030220@gmail.com>
2026-05-04 22:19:45 +0900

    chore: jakarta validation의존성 추가

--- src/main/java/roomescape/reservations/dto/request/ReservationRequest.java
+++ src/main/java/roomescape/reservations/dto/request/ReservationRequest.java
@@ -1,9 +1,13 @@
 package roomescape.reservations.dto.request;
 
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public record ReservationRequest(
+        @NotBlank
         String name,
         LocalTime time,
         LocalDate date
